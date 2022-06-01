c866ca726702d11de783676b500a89ccdc999d59
HalaNHasan <hala_nazeeh@yahoo.com>
2022-06-01 23:32:59 +0300

    created getAllReactionByPostId

--- backend/controllers/reaction.js
+++ backend/controllers/reaction.js
@@ -1,9 +1,12 @@
 const connection = require("../models/db");
 
-const addReactionToPost = (req, res) => {
-  const query = `INSERT INTO post_reaction (author_id,pots_id) VALUES (?,?)`;
-  const {post_id} = req.params.id;
+
+//a function that creates a reaction  for a specific post by post_id
+const addReactionToPost = (req, res) => { //! user can react only one time on a post
+  const query = `INSERT INTO post_reaction (author_id,post_id) VALUES (?,?)`;
+  const post_id = req.params.id;
   const author_id=req.token.userId;
+  const data=[author_id,post_id];
   connection.query(query, data, (error, result) => {
     if (error) {
    return res.status(500).json({
@@ -21,6 +24,29 @@ const addReactionToPost = (req, res) => {
 };
 
 
+//a function that gets all reactions related to a specific post by post_id 
+const getAllReactionByPostId = (req, res) => {
+    const query = `SELECT * FROM post_reaction WHERE post_id=?`;
+    const post_id = req.params.id;
+    const data=[post_id];
+    connection.query(query, data, (error, result) => {
+      if (error) {
+     return res.status(500).json({
+          success: false,
+          massage: "server error",
+          error: error,
+        });
+      }
+      res.status(201).json({
+        success: true,
+        massage: `all reactions for post_id ${post_id}`,
+        result: result,
+      });
+    });
+  };
+
+
 module.exports={
     addReactionToPost,
+    getAllReactionByPostId,
 }
