d3feb0cafd903d480a32430cb25a3e72c1e5d634
HalaNHasan <hala_nazeeh@yahoo.com>
2022-06-01 15:41:24 +0300

    created server

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+//to create the server
+require("dotenv").config();
+const express=require("express");
+const app=express();
+const db=require("./models/db");
+const cors=require("cors");
+
+app.use(express.json());
+app.use(cors());
+const PORT=process.env.PORT;
+
+//require routers
+// const roleRouter=require("./routes/role");
+
+
+
+//routers endpoint
+// app.use("/role",roleRouter);
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+})
