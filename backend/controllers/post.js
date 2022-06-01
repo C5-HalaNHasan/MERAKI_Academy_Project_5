e44e46561569af9ba867309d2108bb946951e551
AhmadAlkhadraa <ahmad.a.alkhadra@gmail.com>
2022-06-01 21:37:23 +0300

    getUserPosts

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -22,10 +22,34 @@ const createPost = (req, res) => {
   });
 };
 
+//create function to get the current user posts
+const getUserPosts =(req,res)=>{
+    const author_id = req.token.userId;
+    const query = `SELECT * FROM post WHERE author_id = ?`;
+    const data = [author_id];
+    connection.query(query,data,(error,result)=>{
+        if(error){
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+              });
+        }
+        res.status(201).json({
+            success: true,
+            message: `All posts for userId => ${author_id}`,
+            result: result,
+          });
+    })
 
+}
+
+// // create function to update post that belong to the current user
+// const updatePostById =(req,res)=>{
+// // const {postText,}
+// }
 
 module.exports={
     createPost,
-    // getUserPosts,
+    getUserPosts,
 
 }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const postRouter = express.Router();
-const {createPost}= require("../controllers/post")
+const {createPost,getUserPosts}= require("../controllers/post")
 
 const {authentication}=require("../middlewares/authentication");
 //endpoint for POST request ==> http://localhost:5000/post==> createPost
 postRouter.post("/",authentication,createPost);
+
+//endpoint for GET request ==> http://localhost:5000/post==> getUserPosts
+postRouter.get("/",authentication,getUserPosts);
 
 module.exports=postRouter;
