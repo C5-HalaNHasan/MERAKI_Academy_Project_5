b8e25b60c3fb5ef66506af6528d5f4267acdffad
HalaNHasan <hala_nazeeh@yahoo.com>
2022-06-02 21:55:02 +0300

    created addReactionToPost & its endpoint

--- backend/controllers/reaction.js
+++ backend/controllers/reaction.js
@@ -22,8 +22,8 @@ const getAllPostsReactions = (req, res) => {
 
 //a function that creates a reaction  for a specific post by post_id
 const addReactionToPost = (req, res) => { //! user can react only one time on a post
-  const query = `SELECT * FROM post_reaction WHERE author_id=? AND post_id=? AND isDeleted=0`;
-  const query1 = `INSERT INTO post_reaction (author_id,post_id) VALUES (?,?)`;
+  const query = `SELECT * FROM post_reaction WHERE author_id=? AND post_id=?`;
+  const query1 = `INSERT INTO post_reaction (author_id,post_id) VALUES (?,?)`; 
   const post_id = req.params.id;
   const author_id=req.token.userId;
   const data=[author_id,post_id];
@@ -37,7 +37,8 @@ const addReactionToPost = (req, res) => { //! user can react only one time on a 
            error: error,
          });
        };
-       if(result.length=0){
+       console.log({inside_add_reaction:result})
+       if(result.length==0){
          connection.query(query1, data, (error1, result1) => {
            if (error1) {
           return res.status(500).json({
@@ -52,37 +53,71 @@ const addReactionToPost = (req, res) => { //! user can react only one time on a 
              result: result1,
            });
          });
-       }else{
-        res.status(403).json({
-          success: false,
-          massage: "you have already reacted to this post",
+       }else if(result[0].isDeleted==1){
+         const query3 = `UPDATE post_reaction SET isDeleted=0 WHERE author_id=? AND post_id=?`; //! to set from isDeleted=1 to isDeleted=0 
+         connection.query(query3, data, (error3, result3) => {
+          if (error3) {
+         return res.status(500).json({
+              success: false,
+              massage: "server error",
+              error: error3,
+            });
+          }
+          res.status(201).json({
+            success: true,
+            massage: "reaction created successfully",
+            result: result3,
+          });
         });
-       }
+        }else{
+          res.status(403).json({
+            success: false,
+            massage: "you have already reacted to this post",
+          });
+        }
   })
 };
 
 
-
-
   //a function that removes a rection from a specific post by post_id 
 const removeReactionFromPost = (req, res) => {
-    // const query = `UPDATE  post_reaction SET isDeleted=1 WHERE id=? AND user_id=?`;
-    // const post_id = req.params.id;
-    // const data=[post_id];
-    // connection.query(query, data, (error, result) => {
-    //   if (error) {
-    //  return res.status(500).json({
-    //       success: false,
-    //       massage: "server error",
-    //       error: error,
-    //     });
-    //   }
-    //   res.status(201).json({
-    //     success: true,
-    //     massage: `all reactions for post_id ${post_id}`,
-    //     result: result,
-    //   });
-    // });
+  const query = `SELECT * FROM post_reaction WHERE author_id=? AND post_id=? AND isDeleted=0`;
+  const query1 = `UPDATE post_reaction SET isDeleted=1 WHERE author_id=? AND post_id=?`;
+  const post_id = req.params.id;
+  const author_id=req.token.userId;
+  const data=[author_id,post_id];
+
+  //first check if the user has previously reacted to the post:
+  connection.query(query,data,(error,result)=>{
+    if (error) {
+      return res.status(500).json({
+           success: false,
+           massage: "server error",
+           error: error,
+         });
+       };
+       if(result.length!=0){
+         connection.query(query1, data, (error1, result1) => {
+           if (error1) {
+          return res.status(500).json({
+               success: false,
+               massage: "server error",
+               error: error1,
+             });
+           }
+           res.status(201).json({
+             success: true,
+             massage: "reaction removed successfully",
+             result: result1,
+           });
+         });
+       }else{
+        res.status(403).json({
+          success: false,
+          massage: "you haven't reacted to this post yet!",
+        });
+       }
+  })
   };
 
 
