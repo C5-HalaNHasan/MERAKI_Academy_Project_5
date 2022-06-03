1fc395def3df8713eab19922784b887a3a2a23da
MotazSoman <motaz.soman@gmail.com>
2022-06-03 23:31:15 +0300

    Message REDUCERS

--- frontend/src/components/redux/reducers/message/index.js
+++ frontend/src/components/redux/reducers/message/index.js
@@ -1,13 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const messageSlice=createSlice({
-    name:"message",
-    initialState:{
+const messageSlice = createSlice({
+  name: "message",
+  initialState: {
+    messages: []
+  },
+  reducers: {
+    setAllMessages: (state, action) => {
+      state.messages = action.payload;
     },
-    reducers:{
-
+    addToMessages: (state, action) => {
+      state.messages.push(action.payload);
+    },
+    removeFromMessages: (state, action) => {
+      state.messages = state.messages.filter((message, index) => {
+        return message.id != action.payload;
+      });
     }
+  }
 });
 
-export const {}=messageSlice.actions;
+export const {setAllMessages,addToMessages,removeFromMessages} = messageSlice.actions;
 export default messageSlice.reducer;
