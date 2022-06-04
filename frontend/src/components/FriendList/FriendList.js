3d46ae16c45c334596cea750ab121fb83a944dd8
MotazSoman <motaz.soman@gmail.com>
2022-06-04 12:16:11 +0300

    friendlist from moutaz

--- frontend/src/components/FriendList/FriendList.js
+++ frontend/src/components/FriendList/FriendList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./friendList.css";
-
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setUserFriends } from "../redux/reducers/user/index";
 const FriendList = () => {
-    return (
-        <div className="friendListComponent">
-        friendListComponent
-        </div>
-    );
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => {
+    return { token: state.user.token };
+  });
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [img, setImg] = useState("");
+  const getAllFriends=async()=>{
+      const response= await axios.get("http://localhost:5000/user/friends",{
+        headers:{
+            Authorization:token
+        } 
+      })
+      console.log(response); 
+
+  }
+  useEffect(()=>{
+      getAllFriends()
+  },[])
+  return <div className="friendListComponent">friendListComponent</div>;
 };
 
 export default FriendList;
