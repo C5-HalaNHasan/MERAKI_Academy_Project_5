4c8837a77e24671884bff5a67e0fa4956a331fed
HalaNHasan <hala_nazeeh@yahoo.com>
2022-06-04 22:58:23 +0300

    updates on messageRouter

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const messageRouter = express.Router();
 
-const {getAllUserMessages,sendMessageToUserById,getAllMessagesFromUserById,removeSentMessageById}= require("../controllers/comment");
+const {getAllUserMessages,sendMessageToUserById,getAllMessagesFromUserById,removeSentMessageById}= require("../controllers/message");
 
 const {authentication}=require("../middlewares/authentication");
 const {authorization} = require("../middlewares/authorization")
@@ -9,13 +9,13 @@ const {authorization} = require("../middlewares/authorization")
 //endpoint for GET request ==> http://localhost:5000/message==> getAllUserMessages
 messageRouter.get("/",authentication,getAllUserMessages);
 
-//endpoint for POST request ==> http://localhost:5000/messag/:id==> sendMessageToUserById
+//endpoint for POST request ==> http://localhost:5000/message/:id==> sendMessageToUserById
 messageRouter.post("/:id",authentication,sendMessageToUserById);
 
-//endpoint for GET request ==> http://localhost:5000/messag/:id==> getAllMessagesFromUserById
+//endpoint for GET request ==> http://localhost:5000/message/:id==> getAllMessagesFromUserById
 messageRouter.get("/:id",authentication,getAllMessagesFromUserById);
 
-//endpoint for PUT request ==> http://localhost:5000/messag/:id==> removeSentMessageById
+//endpoint for PUT request ==> http://localhost:5000/message/:id==> removeSentMessageById
 messageRouter.put("/:id",authentication,removeSentMessageById);
 
 
