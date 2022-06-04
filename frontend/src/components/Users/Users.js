bbc1ad4ef88cff0c9716af137d46398a7c89f4b0
HalaNHasan <hala_nazeeh@yahoo.com>
2022-06-04 11:13:01 +0300

    updated Users component to take two props

--- frontend/src/components/Users/Users.js
+++ frontend/src/components/Users/Users.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import "./users.css";
 
-const Users = () => {
+//Users component will take two props:type(search or friendlist) & name (name of the searched user)
+const Users = ({type,name}) => {
     return (
         <div className="usersComponent">
         usersComponent
