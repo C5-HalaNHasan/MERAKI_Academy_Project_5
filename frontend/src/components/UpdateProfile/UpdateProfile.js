c17ea32e71faa06f5ca8c6de1ecf640d10637b95
MotazSoman <motaz.soman@gmail.com>
2022-06-09 00:09:10 +0300

    cnovert the date and add button update  in user info component , make states in update profile component

--- frontend/src/components/UpdateProfile/UpdateProfile.js
+++ frontend/src/components/UpdateProfile/UpdateProfile.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./updateProfile.css";
 
 const UpdateProfile = () => {
-    return (
-        <div className="updateProfileComponent">
-        updateProfileComponent
-        </div>
-    );
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [hashedPassword, setHashedPassword] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [country, setCountry] = useState("");
+    const [profileImg, setProfileImg] = useState("");
+    const [coverImg, setCoverImg] = useState("");
+    const [isPrivate, setIsPrivate] = useState(0);
+    const dispatch = useDispatch();
+    const { currentUserInfo } = useSelector((state) => {
+      return { currentUserInfo: state.user.currentUserInfo };
+    })
+  return <div className="updateProfileComponent">updateProfileComponent</div>;
 };
 
 export default UpdateProfile;
